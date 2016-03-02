b1d8b1be77d9b58881a3fd58e37bcbaddfa16234
Matt Odden <locke105@gmail.com>
2016-03-02 00:48:46 -0600

    Add base case feature to shoot for

---
+++ specs/base_case.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+
+# It really should be this simple.
+import saturn
+
+# define my request
+spec = {'name': 'my_vm_instance',
+        'image': 'http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img'}
+
+# request/boot - should throw error if we messed up
+my_vm = saturn.boot_vm(spec)
+
+# check status
+print my_vm.status
+
+# wait for boot/networking etc
+my_vm.wait_until(my_vm.ACTIVE)
+
+# get connection info
+net_info = my_vm.network_info
+
+# do something with your VM
+#do_stuff(my_vm)
+
+# done now
+my_vm.destroy()
+
+# wait until its gone
+my_vm.wait_until(my_vm.DELETED)
+
+
+
+
+# maybe someday
+stretch_goals_active = False
+
+if stretch_goals_active:
+    # get some metrics for auditing
+    total_runtime = my_vm.deleted_at - my_vm.created_at
+    print total_runtime
